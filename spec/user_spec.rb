c8734b58093f2b0ffc9b7a002bfb58b8816d2a28
Luis Munoz <thecaber@hotmail.com>
2014-06-05 10:43:10 -0400

    user spec

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -2,9 +2,36 @@ require 'spec_helper'
 
 describe User do
 
-  describe "#initialize" do
-    it "should properly initialize with an username"
+
+
+  before :each do
+    @user = User.new("Luis")
   end
+
+  describe "#initialize" do
+    it "should properly initialize with an username" do
+      expect(@user).to be_kind_of(User)
+    end
+
+    it "should correct name" do
+      expect(@user.name).to eql("Luis")
+    end
+
+  end
+
+  describe "respond to method" do
+    it "should respond to #name" do
+            expect(@user).to respond_to(:add_post)
+    end
+
+    it "should respond to #name" do
+            expect(@user).to respond_to(:list)
+    end
+
+    it "should respond to #name" do
+            expect(@user).to respond_to(:find_post_by_title)
+    end
+   end
 
   describe "#add_post" do
     it "should add a post to the posts list"
