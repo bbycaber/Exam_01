b7fc49500db143cefa79a1ef6a23a1c02c321158
Luis Munoz <thecaber@hotmail.com>
2014-06-05 10:41:23 -0400

    add load yaml file

--- lib/post.rb
+++ lib/post.rb
@@ -3,10 +3,26 @@ class Post
 	attr_reader :title,:text, :date,:user
 
 	def initialize *params
+	  if(params.size ==4)
 		@title = params[0]
 		@text = params[1]
 		@date = params[2]
 		@user = params[3]
-	 
+	 else
+	 	post = load_file(params[0])
+	 	@title = post.title
+		@text = post.text
+		@date = post.date
+		@user = post.user
+	
+	  end
+	end
+
+
+	private 
+
+	def load_file file
+		YAML.load_file(file)
+	end
 
 end
