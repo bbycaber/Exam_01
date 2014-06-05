476ea563e947302969c2c7c6abbb943d36484861
Luis Munoz <thecaber@hotmail.com>
2014-06-05 10:40:12 -0400

    add constructor user and methods

--- lib/user.rb
+++ lib/user.rb
@@ -1,3 +1,19 @@
 class User
 
+	attr_reader :name,:posts
+
+	def initialize name
+		@name = name
+		posts = []
+	end
+
+	def add_post
+	end
+
+	def list
+	end
+
+	def find_post_by_title title 
+	end
+
 end
